4f3de3f33d0488059354d29c56ee00124d40c34b
AlvinSeville7cf <alvinseville7cf@gmail.com>
2021-05-22 20:58:12 +1000

    check version script added

---
+++ scripts/check_version.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+declare -i SUCCESS=0
+declare -i MISSING_VERSION_IN_LABEL_STATUS=1
+declare -i MISSING_VERSION_IN_CSPROJ_STATUS=2
+declare -i VERSIONS_ARE_NOT_EQUAL_STATUS=2
+
+declare labels="$1"
+
+VERSION="$(echo -e "$labels" | grep 'v.*' | head -n 1)"
+if [[ -z "$VERSION" ]]
+then
+    echo "Missing label with version specified for pull request."
+    exit "$MISSING_VERSION_IN_LABEL_STATUS"
+fi
+
+if [[ "$(cat "$CSPROJ_PATH")" =~ \<Version\>(.*)\</Version\> ]]
+then
+    [[ "${BASH_REMATCH[1]}" == "$VERSION" ]] || {
+        echo "Version specified with pull request label is not equal to version specified in C# project."
+        exit "$VERSIONS_ARE_NOT_EQUAL_STATUS"
+    }
+else
+    echo "Missing label with version specified for C# project."
+    exit "$MISSING_VERSION_IN_CSPROJ_STATUS"
+fi
+
+echo "VERSION=$VERSION" >> "$GITHUB_ENV"
+exit "$SUCCESS"
