0e59e69ddea75117b93cb70517f8ba2e5ea78b60
AlvinSeville7cf <alvinseville7cf@gmail.com>
2021-05-23 21:54:14 +1000

    TestCase used instead of Test in attribute tests

--- CommandLineArgumentParser.Tests/Attributes/FlagAttributeTests.cs
+++ CommandLineArgumentParser.Tests/Attributes/FlagAttributeTests.cs
@@ -8,18 +8,12 @@ namespace CommandLineArgumentParser.Tests.Attributes
     [TestFixture]
     public class FlagAttributeTests
     {
-        [Test]
-        public void Expect_ArgumentNullException_When_NameIsNull()
+        [TestCase(null)]
+        [TestCase("")]
+        public void Expect_ArgumentNullException_When_NameIsNull(string name)
         {
-            Assert.Throws<ArgumentNullException>(() => new FlagAttribute(null),
-                $"{nameof(ArgumentNullException)} expected when name parameter was null.");
-        }
-
-        [Test]
-        public void Expect_ArgumentNullException_When_NameIsEmpty()
-        {
-            Assert.Throws<ArgumentNullException>(() => new FlagAttribute(string.Empty),
-                $"{nameof(ArgumentNullException)} expected when name parameter was empty.");
+            Assert.Throws<ArgumentNullException>(() => new FlagAttribute(name),
+                $"{nameof(ArgumentNullException)} expected when name parameter was null or empty.");
         }
 
         [Test]
--- CommandLineArgumentParser.Tests/Attributes/OptionAttributeTests.cs
+++ CommandLineArgumentParser.Tests/Attributes/OptionAttributeTests.cs
@@ -8,18 +8,12 @@ namespace CommandLineArgumentParser.Tests.Attributes
     [TestFixture]
     public class OptionAttributeTests
     {
-        [Test]
-        public void Expect_ArgumentNullException_When_NameIsNull()
+        [TestCase(null)]
+        [TestCase("")]
+        public void Expect_ArgumentNullException_When_NameIsNull(string name)
         {
-            Assert.Throws<ArgumentNullException>(() => new OptionAttribute(null),
-                $"{nameof(ArgumentNullException)} expected when name parameter was null.");
-        }
-
-        [Test]
-        public void Expect_ArgumentNullException_When_NameIsEmpty()
-        {
-            Assert.Throws<ArgumentNullException>(() => new OptionAttribute(string.Empty),
-                $"{nameof(ArgumentNullException)} expected when name parameter was empty.");
+            Assert.Throws<ArgumentNullException>(() => new OptionAttribute(name),
+                $"{nameof(ArgumentNullException)} expected when name parameter was null or empty.");
         }
 
         [Test]
