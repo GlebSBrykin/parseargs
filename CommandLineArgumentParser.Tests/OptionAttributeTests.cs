4fb3da8387b4e3525417c5c97d05bdb8333324ca
AlvinSeville7cf <alvinseville7cf@gmail.com>
2021-05-23 20:32:43 +1000

    Unit tests for OptionAttribute added

---
+++ CommandLineArgumentParser.Tests/OptionAttributeTests.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace CommandLineArgumentParser.Tests
+{
+    [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+    [TestFixture]
+    public class OptionAttributeTests
+    {
+        [Test]
+        public void Expect_ArgumentNullException_When_NameIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new OptionAttribute(null),
+                $"{nameof(ArgumentNullException)} expected when name parameter was null.");
+        }
+
+        [Test]
+        public void Expect_ArgumentNullException_When_NameIsEmpty()
+        {
+            Assert.Throws<ArgumentNullException>(() => new OptionAttribute(string.Empty),
+                $"{nameof(ArgumentNullException)} expected when name parameter was empty.");
+        }
+
+        [Test]
+        public void Expect_NoException_When_NameIsNotNullAndNotEmpty()
+        {
+            Assert.DoesNotThrow(() => new OptionAttribute("--flag"),
+                "No exception expected when name parameter was not null and not empty.");
+        }
+    }
+}
