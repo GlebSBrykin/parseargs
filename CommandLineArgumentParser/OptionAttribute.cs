e6aeb669bc75af421b69f894d4d7170b689b2681
AlvinSeville7cf <alvinseville7cf@gmail.com>
2021-05-23 17:35:02 +1000

    Parsing simplified and completed: - Type property removed in OptionAttribute

--- CommandLineArgumentParser/OptionAttribute.cs
+++ CommandLineArgumentParser/OptionAttribute.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace CommandLineArgumentParser
 {
@@ -7,22 +6,13 @@ namespace CommandLineArgumentParser
     public class OptionAttribute : Attribute
     {
         public string Name { get; }
-        public Type Type { get; }
 
-        public OptionAttribute(string name, Type type)
+        public OptionAttribute(string name)
         {
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentNullException(nameof(name), $"{nameof(name)} can't be null or empty");
-            if (type is null)
-                throw new ArgumentNullException(nameof(type), $"{nameof(type)} can't be null");
 
             Name = name;
-
-            IList<Type> supportedTypes = new List<Type>() { typeof(string), typeof(int), typeof(float) };
-            if (!supportedTypes.Contains(type))
-                throw new ArgumentException($"{nameof(type)} must be typeof(string), typeof(int) or typeof(float)", nameof(type));
-
-            Type = type;
         }
     }
 }
--- CommandLineArgumentParser/Parser.cs
+++ CommandLineArgumentParser/Parser.cs
@@ -8,9 +8,9 @@ namespace CommandLineArgumentParser
     public class Parser<T>
         where T : new()
     {
-        public IEnumerable<string> Args { get; }
+        public IList<string> Args { get; }
 
-        public Parser(IEnumerable<string> args)
+        public Parser(IList<string> args)
         {
             if (args is null)
                 throw new ArgumentNullException(nameof(args), $"{nameof(args)} can't be null");
@@ -25,15 +25,28 @@ namespace CommandLineArgumentParser
             if (!Attribute.GetCustomAttributes(type).Any(attr => attr is ParsableAttribute))
                 throw new ArgumentException($"{nameof(T)} must have ParsableAttribute", nameof(T));
 
-            FieldInfo[] fields = type.GetFields();
-            PropertyInfo[] properties = type.GetProperties();
+            FieldInfo[] fieldInfos = type.GetFields();
+            PropertyInfo[] propertyInfos = type.GetProperties();
 
             try
             {
-                foreach (var field in fields)
-                    FieldHasCorrectAttributes(field);
-                foreach (var property in properties)
-                    PropertyHasCorrectAttributes(property);
+                foreach (var info in fieldInfos)
+                    FieldHasCorrectAttributes(info);
+                foreach (var info in propertyInfos)
+                    PropertyHasCorrectAttributes(info);
+
+                IDictionary<string, Type> nameToTypeMap = CreateDictionaryForAllMembers(fieldInfos, propertyInfos);
+
+                for (int i = 0; i < Args.Count; i++)
+                {
+                    string flagOrOption = Args[i];
+                    string flagOrOptionValue = i + 1 < Args.Count ? Args[i + 1] : string.Empty;
+
+                    if (nameToTypeMap.TryGetValue(flagOrOption, out Type flagOrOptionType))
+                        ParseFlagOrOption(ref i, result, flagOrOption, flagOrOptionValue, flagOrOptionType);
+                    else
+                        throw new ArgumentException($"{flagOrOption} flag/option is not supported", nameof(Args));
+                }
             }
             catch (ArgumentException e)
             {
@@ -43,118 +56,173 @@ namespace CommandLineArgumentParser
             return result;
         }
 
+        private static void ParseFlagOrOption(ref int i, T instance, string flagOrOption, string flagOrOptionValue, Type flagOrOptionType)
+        {
+            if (flagOrOptionType == typeof(bool))
+            {
+                if (bool.TryParse(flagOrOptionValue, out bool boolValue))
+                {
+                    PutValue(instance, flagOrOption, boolValue);
+                    i++;
+                }
+                else
+                    throw new ArgumentException($"{flagOrOption} option has {flagOrOptionType.Name} type but passed value has another type", nameof(flagOrOption));
+            }
+            else if (flagOrOptionType == typeof(int))
+            {
+                if (int.TryParse(flagOrOptionValue, out int intValue))
+                {
+                    PutValue(instance, flagOrOption, intValue);
+                    i++;
+                }
+                else
+                    throw new ArgumentException($"{flagOrOption} option has {flagOrOptionType.Name} type but passed value has another type", nameof(flagOrOption));
+            }
+            else if (flagOrOptionType == typeof(float))
+            {
+                if (int.TryParse(flagOrOptionValue, out int floatValue))
+                {
+                    PutValue(instance, flagOrOption, floatValue);
+                    i++;
+                }
+                else
+                    throw new ArgumentException($"{flagOrOption} option has {flagOrOptionType.Name} type but passed value has another type", nameof(flagOrOption));
+            }
+            else if (flagOrOptionType == typeof(string))
+            {
+                PutValue(instance, flagOrOption, flagOrOptionValue);
+                i++;
+            }
+            else if (flagOrOptionType is null)
+                PutValue(instance, flagOrOption, true);
+            else
+                throw new ArgumentException($"{flagOrOption} flag/option has unsopported type {flagOrOptionType.Name}", nameof(flagOrOption));
+        }
+
+        private static void PutValue(T instance, string name, object value)
+        {
+            Type type = instance.GetType();
+
+            IEnumerable<FieldInfo> fieldInfos = type.GetFields().Where(field => field.GetCustomAttributes<FlagAttribute>().Any(attr => attr.Name == name) ||
+                field.GetCustomAttributes<OptionAttribute>().Any(attr => attr.Name == name));
+            if (fieldInfos.Any())
+                fieldInfos.First().SetValue(instance, value);
+            else
+            {
+                IEnumerable<PropertyInfo> propertyInfos = type.GetProperties().Where(property => property.GetCustomAttributes<FlagAttribute>().Any(attr => attr.Name == name) ||
+                property.GetCustomAttributes<OptionAttribute>().Any(attr => attr.Name == name));
+                if (propertyInfos.Any())
+                    propertyInfos.First().SetValue(instance, value);
+            }
+        }
+
         private static void FieldHasCorrectAttributes(FieldInfo fieldInfo)
         {
-            IList<FlagAttribute> flagAttributes = fieldInfo.GetCustomAttributes<FlagAttribute>().ToList();
-            IList<OptionAttribute> optionAttributes = fieldInfo.GetCustomAttributes<OptionAttribute>().ToList();
+            IEnumerable<FlagAttribute> flagAttributes = fieldInfo.GetCustomAttributes<FlagAttribute>();
+            IEnumerable<OptionAttribute> optionAttributes = fieldInfo.GetCustomAttributes<OptionAttribute>();
 
-            if (flagAttributes.Count > 0 && optionAttributes.Count > 0)
+            if (flagAttributes.Any() && optionAttributes.Any())
                 throw new ArgumentException($"{fieldInfo.Name} field can't have both {nameof(FlagAttribute)} and {nameof(OptionAttribute)} attributes", nameof(fieldInfo));
 
-            if (flagAttributes.Count > 0)
+            if (flagAttributes.Any())
             {
-                IEnumerable<string> names = flagAttributes.Select(attr => attr.Name);
-                if (names.Distinct().Count() != names.Count())
+                if (ThereAreDublicatingNames(flagAttributes))
                     throw new ArgumentException($"{fieldInfo.Name} field can't have {nameof(FlagAttribute)} with dublicating names", nameof(fieldInfo));
+                if (fieldInfo.FieldType != typeof(bool))
+                    throw new ArgumentException($"{fieldInfo.Name} flag field can't have non-bool type", nameof(fieldInfo));
             }
-            else if (optionAttributes.Count > 0)
+            else if (optionAttributes.Any())
             {
-                IEnumerable<string> names = optionAttributes.Select(attr => attr.Name);
-                if (names.Distinct().Count() != names.Count())
+                if (ThereAreDublicatingNames(optionAttributes))
                     throw new ArgumentException($"{fieldInfo.Name} field can't have {nameof(OptionAttribute)} with dublicating names", nameof(fieldInfo));
-                if (optionAttributes.Select(attr => attr.Type).Distinct().Count() > 1)
-                    throw new ArgumentException($"{fieldInfo.Name} field can't have {nameof(OptionAttribute)} with different types", nameof(fieldInfo));
             }
         }
 
         private static void PropertyHasCorrectAttributes(PropertyInfo propertyInfo)
         {
-            IList<FlagAttribute> flagAttributes = propertyInfo.GetCustomAttributes<FlagAttribute>().ToList();
-            IList<OptionAttribute> optionAttributes = propertyInfo.GetCustomAttributes<OptionAttribute>().ToList();
-
-            if (flagAttributes.Count > 0 && optionAttributes.Count > 0)
-                throw new ArgumentException($"{propertyInfo.Name} field can't have both {nameof(FlagAttribute)} and {nameof(OptionAttribute)} attributes", nameof(propertyInfo));
-
-            if (flagAttributes.Count > 0)
-            {
-                IEnumerable<string> names = flagAttributes.Select(attr => attr.Name);
-                if (names.Distinct().Count() != names.Count())
-                    throw new ArgumentException($"{propertyInfo.Name} field can't have {nameof(FlagAttribute)} with dublicating names", nameof(propertyInfo));
-            }
-            else if (optionAttributes.Count > 0)
-            {
-                IEnumerable<string> names = optionAttributes.Select(attr => attr.Name);
-                if (names.Distinct().Count() != names.Count())
-                    throw new ArgumentException($"{propertyInfo.Name} field can't have {nameof(OptionAttribute)} with dublicating names", nameof(propertyInfo));
-                if (optionAttributes.Select(attr => attr.Type).Distinct().Count() > 1)
-                    throw new ArgumentException($"{propertyInfo.Name} field can't have {nameof(OptionAttribute)} with different types", nameof(propertyInfo));
-            }
-        }
-
-        private static IDictionary<string, SupportedType> CreateDictionaryForFields(IEnumerable<FieldInfo> fieldInfos)
-        {
-            IDictionary<string, SupportedType> result = new Dictionary<string, SupportedType>();
-            foreach (var fieldInfo in fieldInfos)
-            {
-                KeyValuePair<string, SupportedType> pair = CreateKeyValuePairForField(fieldInfo);
-                result.Add(pair.Key, pair.Value);
-            }
-            return result;
-        }
-
-        private static IDictionary<string, SupportedType> CreateDictionaryForProperties(IEnumerable<PropertyInfo> propertyInfos)
-        {
-            IDictionary<string, SupportedType> result = new Dictionary<string, SupportedType>();
-            foreach (var fieldInfo in propertyInfos)
-            {
-                KeyValuePair<string, SupportedType> pair = CreateKeyValuePairForProperty(fieldInfo);
-                result.Add(pair.Key, pair.Value);
-            }
-            return result;
-        }
-
-        private static KeyValuePair<string, SupportedType> CreateKeyValuePairForField(FieldInfo fieldInfo)
-        {
-            IEnumerable<string> flagAttributeNames = fieldInfo.GetCustomAttributes<FlagAttribute>().Select(attr => attr.Name);
-
-            IEnumerable<OptionAttribute> optionAttributes = fieldInfo.GetCustomAttributes<OptionAttribute>();
-            IEnumerable<string> optionAttributeNames = optionAttributes.Select(attr => attr.Name);
-
-            IDictionary<Type, SupportedType> typesToSupportedTypes = new Dictionary<Type, SupportedType>()
-            {
-                [typeof(string)] = SupportedType.String,
-                [typeof(int)] = SupportedType.Int,
-                [typeof(float)] = SupportedType.Float
-            };
-
-            if (flagAttributeNames.Any())
-                return new KeyValuePair<string, SupportedType>(flagAttributeNames.First(), SupportedType.None);
-            else if (optionAttributeNames.Any())
-                return new KeyValuePair<string, SupportedType>(optionAttributeNames.First(), typesToSupportedTypes[optionAttributes.First().Type]);
-
-            throw new ArgumentException($"field have incorrect {nameof(FlagAttribute)} and (or) {nameof(OptionAttribute)} attributes", nameof(fieldInfo));
-        }
-
-        private static KeyValuePair<string, SupportedType> CreateKeyValuePairForProperty(PropertyInfo propertyInfo)
-        {
-            IEnumerable<string> flagAttributeNames = propertyInfo.GetCustomAttributes<FlagAttribute>().Select(attr => attr.Name);
-
+            IEnumerable<FlagAttribute> flagAttributes = propertyInfo.GetCustomAttributes<FlagAttribute>();
             IEnumerable<OptionAttribute> optionAttributes = propertyInfo.GetCustomAttributes<OptionAttribute>();
-            IEnumerable<string> optionAttributeNames = optionAttributes.Select(attr => attr.Name);
 
-            IDictionary<Type, SupportedType> typesToSupportedTypes = new Dictionary<Type, SupportedType>()
+            if (flagAttributes.Any() && optionAttributes.Any())
+                throw new ArgumentException($"{propertyInfo.Name} property can't have both {nameof(FlagAttribute)} and {nameof(OptionAttribute)} attributes", nameof(propertyInfo));
+
+            if (flagAttributes.Any())
             {
-                [typeof(string)] = SupportedType.String,
-                [typeof(int)] = SupportedType.Int,
-                [typeof(float)] = SupportedType.Float
-            };
+                if (ThereAreDublicatingNames(flagAttributes))
+                    throw new ArgumentException($"{propertyInfo.Name} property can't have {nameof(FlagAttribute)} with dublicating names", nameof(propertyInfo));
+                if (propertyInfo.GetMethod?.ReturnType != typeof(bool))
+                    throw new ArgumentException($"{propertyInfo.Name} flag property can't have non-bool type or don't have getter", nameof(propertyInfo));
+            }
+            else if (optionAttributes.Any())
+            {
+                if (ThereAreDublicatingNames(optionAttributes))
+                    throw new ArgumentException($"{propertyInfo.Name} property can't have {nameof(OptionAttribute)} with dublicating names", nameof(propertyInfo));
+            }
+        }
 
-            if (flagAttributeNames.Any())
-                return new KeyValuePair<string, SupportedType>(flagAttributeNames.First(), SupportedType.None);
-            else if (optionAttributeNames.Any())
-                return new KeyValuePair<string, SupportedType>(optionAttributeNames.First(), typesToSupportedTypes[optionAttributes.First().Type]);
+        private static bool ThereAreDublicatingNames(IEnumerable<FlagAttribute> attributes)
+        {
+            IEnumerable<string> names = attributes.Select(attr => attr.Name);
+            return names.Distinct().Count() != names.Count();
+        }
 
-            throw new ArgumentException($"field have incorrect {nameof(FlagAttribute)} and (or) {nameof(OptionAttribute)} attributes", nameof(propertyInfo));
+        private static bool ThereAreDublicatingNames(IEnumerable<OptionAttribute> attributes)
+        {
+            IEnumerable<string> names = attributes.Select(attr => attr.Name);
+            return names.Distinct().Count() != names.Count();
+        }
+
+        private static IDictionary<string, Type> CreateDictionaryForAllMembers(IEnumerable<FieldInfo> fieldInfos, IEnumerable<PropertyInfo> propertyInfos)
+        {
+            IDictionary<string, Type> nameToTypeMap = new Dictionary<string, Type>();
+
+            foreach (var info in fieldInfos)
+            {
+                foreach (var pair in CreateDictionaryForField(info))
+                    if (!nameToTypeMap.TryAdd(pair.Key, pair.Value))
+                        throw new ArgumentException($"There are two fields with identical name name {pair.Key} in {nameof(FlagAttribute)} and (or) {nameof(OptionAttribute)} attributes", nameof(fieldInfos));
+            }
+
+            foreach (var info in propertyInfos)
+            {
+                foreach (var pair in CreateDictionaryForProperty(info))
+                    if (!nameToTypeMap.TryAdd(pair.Key, pair.Value))
+                        throw new ArgumentException($"There are two fields/properties with identical name {pair.Key} in {nameof(FlagAttribute)} and (or) {nameof(OptionAttribute)} attributes", nameof(propertyInfos));
+            }
+
+            return nameToTypeMap;
+        }
+
+        private static IDictionary<string, Type> CreateDictionaryForField(FieldInfo info)
+        {
+            IEnumerable<string> flagNames = info.GetCustomAttributes<FlagAttribute>().Select(attr => attr.Name);
+            IEnumerable<string> optionNames = info.GetCustomAttributes<OptionAttribute>().Select(attr => attr.Name);
+
+            IDictionary<string, Type> nameToTypeMap = new Dictionary<string, Type>();
+            if (flagNames.Any())
+                foreach (var name in flagNames)
+                    nameToTypeMap.Add(name, null);
+            else if (optionNames.Any())
+                foreach (var name in optionNames)
+                    nameToTypeMap.Add(name, info.FieldType);
+
+            return nameToTypeMap;
+        }
+
+        private static IDictionary<string, Type> CreateDictionaryForProperty(PropertyInfo info)
+        {
+            IEnumerable<string> flagNames = info.GetCustomAttributes<FlagAttribute>().Select(attr => attr.Name);
+            IEnumerable<string> optionNames = info.GetCustomAttributes<OptionAttribute>().Select(attr => attr.Name);
+
+            IDictionary<string, Type> nameToTypeMap = new Dictionary<string, Type>();
+            if (flagNames.Any())
+                foreach (var name in flagNames)
+                    nameToTypeMap.Add(name, null);
+            else if (optionNames.Any())
+                foreach (var name in optionNames)
+                    nameToTypeMap.Add(name, info.GetMethod?.ReturnType);
+
+            return nameToTypeMap;
         }
     }
 }
